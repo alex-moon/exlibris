633278949a7b740720a3d915e92df856cb5fcfd9
Alex Moon <alex.moon@tangentlabs.co.uk>
2012-03-24 23:50:14 +0000

    Um... I'm not sure what's going on really

--- people/models.py
+++ people/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Person(models.Model):
+    user = models.ForeignKey(User)
     tagline = models.CharField(max_length=1024)
 
 class Author(models.Model):
