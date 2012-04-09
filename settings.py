f5fb0557dbafc710c1dc8fd1d25a20ba35865dd3
Alex Moon <alex.moon@tangentlabs.co.uk>
2012-04-09 23:00:33 +0100

    Alrighty I think I'm getting the hang of it.

--- settings.py
+++ settings.py
@@ -97,9 +97,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'exlibris.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    '/var/www/exlibris/templates',
 )
 
 INSTALLED_APPS = (
--- views.py
+++ views.py
@@ -1,4 +1,9 @@
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def home(request):
-    return HttpResponse("Welcome to exlibris you fucking cunt.")
+    bindings = {
+        'pagetitle' : 'Borrow, buy, loan and sell books to your fellow readers and meet people',
+        'content' : '<article><h1>Welcome!</h1><p><span class="exlibris">exlibris</span> is a community of book-lovers designed\
+                     to increase both circulation of books and interaction of individuals. You\'re gonna love this I swear.</p></article>',
+    }
+    return render_to_response('base.html', bindings)
